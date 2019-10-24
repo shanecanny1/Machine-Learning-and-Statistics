fb060b403f5aa0d3eee1134112f9b9a67dd4407e
shanecanny1 <36367839+shanecanny1@users.noreply.github.com>
2019-10-25 00:27:18 +0100

    Add files via upload
    Updated program to include console input request to enter a number you want the square root of. Also commented on the code.

--- newtonest.py
+++ newtonest.py
@@ -1,8 +1,12 @@
 ## Example of getting a closer estimate ##
+## Code Modified by Shane Canny ##
+## Source: https://tour.golang.org/flowcontrol
+## 25 Oct 2019 ##
 
-val1 = int(input("Enter your inital estimate of the Square Root Value: ")) 
+val1 = int(input("Enter your inital estimate of the Square Root Value: ")) ##Added the functionality to input an initial sqrt guess ##
+val2 = int(input("Enter the Value you want the Square Root of: "))  ## Added the functionality to input an initial value you want to get the sqrt of ## 
 
-def sqrt(x):
+def sqrt(x): ## x here is the value that will be taken fron val2 on line 17, this value will be ise in the while loop ##
     z = val1
     while abs(z*z-x) >= 0.001:
         z-=(z*z-x)/(2*z)
@@ -10,6 +14,6 @@ def sqrt(x):
 
 
 
-z=sqrt(4)
-print (z)
-
+z=sqrt(val2) ## Calling the function ##
+print (z) ## Printing the sqrt result##
+print (z*z) ## Confirming the calculation is correct by squaring the calculated sqrt ##
